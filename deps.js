e47af4d40ef4887c1c138fbe41bee77d13d84314
Mikko <mikko.peratalo@aalto.fi>
2023-02-23 11:44:19 +0200

    jaa a

--- deps.js
+++ deps.js
@@ -1,4 +1,6 @@
 export { serve } from "https://deno.land/std@0.171.0/http/server.ts";
 export { configure, renderFile } from "https://deno.land/x/eta@v2.0.0/mod.ts";
 
-export {postgres} from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+import postgres from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+
+export {postgres};
